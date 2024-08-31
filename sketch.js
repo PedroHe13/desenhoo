2b10b58064474b2474b03fe0205b42f1cf558049
PedroHe13 <phrodriguesdon1301@gmail.com>
2024-08-31 17:38:52 -0300

    adicionando html, js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() { 
+    createCanvas (600, 600); 
+    background("black"); 
+}
+
+function draw() {
+
+stroke("blue");
+fill("red");
+
+if (mouseIs Pressed) { 
+    rect(mousex, mouseY, 20, 35);
+
+}
+
+}
